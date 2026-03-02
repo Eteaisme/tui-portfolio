3e60e6c3925ed9a8cf036c9f10c237017cfc5d99
Elliot <eteaisme@gmail.com>
2026-03-01 23:39:29 -0500

    Split into multiple element objects

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 #include <ftxui/dom/node.hpp>
 #include <ftxui/screen/screen.hpp>
 
-//Dev log(01/03/2026): 
+//Dev log(02/03/2026): 
 // Add about me, footer and navbar. (static for now)
 //Catching up, haven't contribed for a couple days.
 //
 //Added seperators, and we've got to justify, center
-//Seperate into differnt elements
+//Seperate into differnt elements --> ELement, 
 
 
 // Copyright 2020 Arthur Sonzogni. All rights reserved.
@@ -21,29 +21,29 @@
 int main() {
   using namespace ftxui;
 
+
+  Element navbar = hbox({
+          text("home"),
+          separator(),
+          text("projects"),
+          separator(),
+          text("contact")
+          }) 
+  | border | center;
+
+  Element about_me = vbox({
+          text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"),
+          text("ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"),
+          text("ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in"),
+          }) 
+  | border | center;
+
   auto foo = Container::Vertical({});
-  auto navbar = Container::Horizontal({
-                  hbox({
-                        text("home"),
-                        separator(),
-                        text("projects"),
-                        separator(),
-                        text("contact")
-                          }) 
-                  | border
-                  | center
-          });
   auto document = Renderer(foo, [&]{
           return vbox({
+                  navbar,
 
 
-                  vbox({
-                          text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor"),
-                          text("ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"),
-                          text("ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in"),
-                          }) 
-                  | border
-                  | center
 
                   //size here returns terminal size, not exactly sure what I'm piping to...
                   }) | size(HEIGHT, GREATER_THAN, 50);
