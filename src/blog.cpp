69146bb4b10c1abc0e943246b169721937ab770c
Elliot <eteaisme@gmail.com>
2026-09-22 08:32:02 -0400

    added img to src

---
+++ src/blog.cpp
@@ -0,0 +1,5 @@
+#include <string>
+#include <vector>
+    std::vector<std::string> blog_entries{
+        "blog entry 1            ", "blog entry 2", "blog entry 3", "blog entry 4" 
+    };
---
+++ src/projects.cpp
@@ -0,0 +1,19 @@
+#include <string> 
+#include <vector>
+    std::vector<std::string> project_entries{
+        "project 1               ", "project 2", "project 3", "project 4" 
+    };
+
+    std::vector<std::string> project_descriptions {
+        "project 1 desc.",  //tui portfolio
+        "project 2 desc.",  //linux conf
+        "project 3 desc.",  //personal site
+        "project 4 desc. ", 
+    };
+
+    std::vector<std::string> blog_content{
+        "article 1 content",  
+        "article 2 content",  
+        "article 3 content",  
+        "article 4 content", 
+    };
